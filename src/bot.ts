0e0ccde05471e8f1d4a95f3aff834f53c6a609ea
TryCatchB <pozdrakovzena@gmail.com>
2025-03-30 17:37:16 +0300

    Вынес создание бота

--- src/bot.ts
+++ src/bot.ts
@@ -1,77 +1,17 @@
-import TelegramBot from "node-telegram-bot-api";
-import dotenv from "dotenv";
-import { createWallet } from "./createWallet";
-import { saveUser } from "./saveUser";
-import { getWalletBalance } from "./getWalletBalance";
-import { isUserRegistered } from "./isUserRegistered";
-import { encryptPrivateKey } from "./encryptPrivateKey";
+import bot from "./botInstance";
+import registerHandler from "./functions/registered/registerHandler";
+import processUserMessage from "./functions/send/processUserMessage";
+import showWalletBalance from "./functions/balance/showWalletBalance";
+import greetingsMessage from "./functions/greetings/greetingsMessage";
 
-dotenv.config();
+bot.on("message", processUserMessage);
 
-const token = process.env.BOT_TOKEN;
+bot.onText(/\/register/, registerHandler);
 
-if (!token) {
-  console.error(
-    "Токен не найден! Убедитесь, что BOT_TOKEN задан в .env файле."
-  );
-  process.exit(1);
-}
+bot.onText(/\/show/, showWalletBalance);
 
-const bot = new TelegramBot(token, { polling: true });
-
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
-
-  if (!text?.startsWith("/")) {
-    bot.sendMessage(chatId, `Ты написал: ${text}`);
-  }
-});
-
-bot.onText(/\/register/, async (msg) => {
-  const chatId = msg.chat.id;
-  const username = msg.from?.username || "Неизвестный";
-
-  const isRegistered = await isUserRegistered(chatId);
-
-  if (isRegistered) {
-    bot.sendMessage(chatId, "Ты уже зарегистрирован! 🚀");
-    return;
-  }
-
-  const result = await createWallet();
-
-  if (result) {
-    const { privateKey, wallet } = result;
-
-    const encryptedPrivateKey = encryptPrivateKey(privateKey);
-
-    await saveUser(chatId, username, wallet.address, encryptedPrivateKey);
-
-    bot.sendMessage(
-      chatId,
-      `Твой кошелек был создан: ${wallet.address.base58}`
-    );
-  } else {
-    bot.sendMessage(chatId, "Не удалось создать кошелек. Попробуй позже.");
-  }
-});
-
-bot.onText(/\/show/, async (msg) => {
-  const chatId = msg.chat.id;
-
-  const walletBalance = await getWalletBalance();
-  bot.sendMessage(chatId, `Your balance: ${walletBalance}`);
-});
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "Welcome to p2p exchange telegram bot, a decentralized P2P exchange bot that allows users to securely trade cryptocurrency for fiat without relying on centralized platforms."
-  );
-});
+bot.onText(/\/start/, greetingsMessage);
 
 bot.on("polling_error", (error) => {
-  console.error("Ошибка при опросе бота:", error);
+  console.error("❌ Ошибка опроса бота:", error);
 });
