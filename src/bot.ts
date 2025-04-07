dfdb8c2f4548064aa0ca206fb6e5ecc540bb8445
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-04-07 22:17:31 +0300

    Add new commands

--- src/bot.ts
+++ src/bot.ts
@@ -3,14 +3,20 @@ import registerHandler from "./functions/registered/registerHandler";
 import processUserMessage from "./functions/send/processUserMessage";
 import showWalletBalance from "./functions/balance/showWalletBalance";
 import greetingsMessage from "./functions/greetings/greetingsMessage";
+import createExchange from "./functions/create/createExchange";
+import sellCrypto from "./functions/send/sellCrypto";
 
 bot.on("message", processUserMessage);
 
-bot.onText(/\/register/, registerHandler);
+bot.onText(/\/createWallet/, registerHandler);
 
-bot.onText(/\/show/, showWalletBalance);
+bot.onText(/\/showBalance/, showWalletBalance);
+
+bot.onText(/\/createExchange/, createExchange);
 
 bot.onText(/\/start/, greetingsMessage);
+
+bot.onText(/\/sellCrypto/, sellCrypto);
 
 bot.on("polling_error", (error) => {
   console.error("❌ Ошибка опроса бота:", error);
