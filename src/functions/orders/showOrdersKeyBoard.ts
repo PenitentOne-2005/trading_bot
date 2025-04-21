0d5e1028bd874310e73d63edba02cde32a4ac552
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-04-21 22:06:30 +0300

    Create showOrdersKeyBoard

---
+++ src/functions/orders/showOrdersKeyBoard.ts
@@ -0,0 +1,13 @@
+import { SendMessageOptions } from "node-telegram-bot-api";
+import { MenuButton } from "../../interface";
+
+export const ordersMenu: SendMessageOptions = {
+  reply_markup: {
+    keyboard: [
+      [{ text: "/buy" }, { text: "/next" }] as MenuButton[],
+      [{ text: "/back" }] as MenuButton[],
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: false,
+  },
+};
