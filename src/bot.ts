81258f92ae46ea623c12355756980e8d09ea6ff6
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-04-27 12:37:12 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
 import bot from "./botInstance";
 import registerHandler from "./functions/registered/registerHandler";
 import processUserMessage from "./functions/send/processUserMessage";
@@ -7,8 +6,6 @@ import greetingsMessage from "./functions/greetings/greetingsMessage";
 import createExchange from "./functions/create/createExchange";
 import buyCrypto from "./functions/send/buyCrypto";
 import showOrders from "./functions/orders/showOrders";
-
-dotenv.config();
 
 bot.on("message", processUserMessage);
 
