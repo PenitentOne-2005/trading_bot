c0bd7e250f583222d4580f34e92dc236f2423709
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-04-27 19:56:00 +0300

    Change import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
+import pkg from "pg";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -10,6 +10,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+const { Pool } = pkg;
 
 const pool = new Pool({
   host: process.env.DB_HOST,
