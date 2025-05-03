26ac0e4fc5511559dbad8c97fa48d2053058a067
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-05-03 16:26:32 +0300

    Update

--- dist/botInstance.js
+++ dist/botInstance.js
@@ -1,6 +1,6 @@
-import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 dotenv.config();
+import TelegramBot from "node-telegram-bot-api";
 const token = process.env.BOT_TOKEN;
 if (!token) {
     console.error("❌ BOT_TOKEN не найден! Убедитесь, что он задан в .env файле.");
