5e11330fe134806a98e5d9ce79e42046fd01ba65
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-05-20 22:00:46 +0300

    Update text

--- src/commandKeyboard.ts
+++ src/commandKeyboard.ts
@@ -8,7 +8,7 @@ export const mainMenu: SendMessageOptions = {
       [{ text: "Всі оголошення" }] as MenuButton[],
       [{ text: "Мої оголошення" }] as MenuButton[],
       [{ text: "Створити оголошення" }] as MenuButton[],
-      [{ text: "Домога" }] as MenuButton[],
+      [{ text: "Допомога" }] as MenuButton[],
     ],
     resize_keyboard: true,
     one_time_keyboard: false,
