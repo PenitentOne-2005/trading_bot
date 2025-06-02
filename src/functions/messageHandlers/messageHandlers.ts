48a4f2982fa7fce62179044fbc316b3675686150
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-02 19:09:58 +0300

    Update paths

--- src/functions/messageHandlers/messageHandlers.ts
+++ src/functions/messageHandlers/messageHandlers.ts
@@ -1,6 +1,6 @@
 import { Message } from "node-telegram-bot-api";
-import MESSAGE_TEXT from "./contentText.js";
-import { selectLanguageBoard } from "./selectLanguageBoard.js";
+import MESSAGE_TEXT from "../../contentText.js";
+import { selectLanguageBoard } from "../../selectLanguageBoard.js";
 import sendMessage from "../sendMessage/sendMessage.js";
 
 const createMessageHandlers = (chatId: number) => {
