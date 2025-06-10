7e31bfa7cc864238d151cd4aface97c875e5feeb
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-10 19:43:52 +0300

    Dist

--- dist/functions/showWallet/showWallet.js
+++ dist/functions/showWallet/showWallet.js
@@ -5,6 +5,6 @@ import sendMessage from "../sendMessage/sendMessage.js";
 const showWallet = async (chatId) => {
     const balance = await getWalletBalance();
     const wallet = getWalletAddress();
-    sendMessage(chatId, `Baш криптовалютний баланс\n Блокчейн: TRON (TRC-20)\n Ваші активи:\n * USDT: ${balance?.usdt} USDT\n * TRX (необхідний для комісій): ${balance?.trx} TRX\n Адреса вашого гаманця:\n ${wallet}\n Доступні дії:`, menu);
+    return sendMessage(chatId, `Baш криптовалютний баланс\n Блокчейн: TRON (TRC-20)\n Ваші активи:\n * USDT: ${balance?.usdt} USDT\n * TRX (необхідний для комісій): ${balance?.trx} TRX\n Адреса вашого гаманця:\n ${wallet}\n Доступні дії:`, menu);
 };
 export default showWallet;
