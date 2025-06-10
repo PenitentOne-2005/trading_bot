d884aad1ce3e9f995795a26ee740a60aca3aa0fc
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-10 20:32:38 +0300

    Add check if table not exists

--- src/functions/saveUser/saveUser.ts
+++ src/functions/saveUser/saveUser.ts
@@ -2,18 +2,31 @@ import { ISaveUser } from "./interface.js";
 import pool from "../../db.js";
 
 const saveUser: ISaveUser = async (data) => {
-  const { chatId, username, address, encryptedPrivateKey } = data;
+  const { chatId, username, address, encryptedKey, iv } = data;
 
   try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        telegram_id BIGINT PRIMARY KEY,
+        username TEXT,
+        wallet_address TEXT,
+        private_key TEXT,
+        iv TEXT
+      );
+    `);
+
     await pool.query(
-      `INSERT INTO users (telegram_id, username, wallet_address, private_key)
-       VALUES ($1, $2, $3, $4)
+      `INSERT INTO users (telegram_id, username, wallet_address, private_key, iv)
+       VALUES ($1, $2, $3, $4, $5)
        ON CONFLICT (telegram_id) DO UPDATE 
        SET wallet_address = EXCLUDED.wallet_address, 
            private_key = EXCLUDED.private_key, 
-           username = EXCLUDED.username;`,
-      [chatId, username, address, encryptedPrivateKey]
+           username = EXCLUDED.username,
+           iv = EXCLUDED.iv;`,
+      [chatId, username, address, encryptedKey, iv]
     );
+
     console.log(`✅ Пользователь ${chatId} сохранен`);
   } catch (error) {
     console.error("❌ Ошибка при сохранении пользователя:", error);
