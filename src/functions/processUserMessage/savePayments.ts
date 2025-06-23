003f76d957caab2694842deca0e60fed7e568d1f
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-23 09:02:18 +0300

    New function

---
+++ src/functions/processUserMessage/savePayments.ts
@@ -0,0 +1,24 @@
+import pool from "../../db";
+
+const savePayments = async (chatId: number, metadata: any) => {
+  try {
+    await pool.query(`
+          CREATE TABLE IF NOT EXISTS payments (
+            id SERIAL PRIMARY KEY,
+            telegram_id BIGINT UNIQUE,
+            metadata TEXT
+          );
+        `);
+    await pool.query(
+      `INSERT INTO users (telegram_id, metadata)
+           VALUES ($1, $2)
+          `,
+      [chatId, metadata]
+    );
+    console.log(`✅ Пользователь ${chatId} сохранён.`);
+  } catch (error) {
+    console.error("❌ Ошибка при сохранении пользователя:", error);
+  }
+};
+
+export default savePayments;
