0c0744020e5e36613ce0053cf03c1c5d90b650fd
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-23 09:03:56 +0300

    add new cases

--- src/functions/processUserMessage/processUserMessage.ts
+++ src/functions/processUserMessage/processUserMessage.ts
@@ -5,6 +5,8 @@ import sendMessage from "../sendMessage/sendMessage.js";
 import { userState } from "../../userState.js";
 import waitingForPrice from "./waitingForPrice.js";
 import waitingForAmount from "./waitingForAmount.js";
+import savePayments from "./savePayments.js";
+import showSummary from "../showSummary/showSummary.js";
 
 const processUserMessage: IProcessUserMessage = async (msg) => {
   const { chat, text } = msg;
@@ -14,45 +16,153 @@ const processUserMessage: IProcessUserMessage = async (msg) => {
 
   const currentState = userState[chatId];
 
-  // 🧠 Сначала проверка на FSM (если есть step)
   if (currentState?.step) {
     switch (currentState.step) {
       case "waitingForPrice": {
-        const props = {
-          userState,
-          currentState,
-          chatId,
-          text,
-        };
-
+        const props = { userState, currentState, chatId, text };
         return waitingForPrice(props);
       }
 
       case "waitingForAmount": {
-        const props = {
-          userState,
-          chatId,
-          text,
-        };
-
+        const props = { userState, chatId, text };
         return waitingForAmount(props);
       }
 
-      default:
-        sendMessage(
+      case "waitingForCard": {
+        if (/^\d{16}$/.test(text)) {
+          const obj = JSON.stringify({ text });
+
+          await savePayments(chatId, obj);
+          return showSummary(chatId, userState, currentState);
+        } else {
+          return sendMessage(
+            chatId,
+            "❌ Помилка! Невірний номер картки.\nНомер банківської картки повинен містити рівно 16 цифр без пробілів або символів.\nБудь ласка, введіть коректний номер карти:",
+            {
+              reply_markup: {
+                inline_keyboard: [[{ text: "Назад", callback_data: "back" }]],
+              },
+            }
+          );
+        }
+      }
+
+      case "waitingForIBAN": {
+        if (/^UA\d{2}\d{6}\d{19}$/.test(text)) {
+          userState[chatId] = {
+            ...userState[chatId],
+            step: "waitingForIPN",
+            IBANdata: {
+              ...(userState[chatId]?.IBANdata || {}),
+              IBAN: text,
+            },
+          };
+
+          return sendMessage(
+            chatId,
+            "Введіть індивідуальний податковий номер (ІПН):",
+            {
+              reply_markup: {
+                inline_keyboard: [
+                  [{ text: "Назад", callback_data: "back" }],
+                  [{ text: "Скасувати", callback_data: "back" }],
+                ],
+              },
+            }
+          );
+        } else {
+          return sendMessage(
+            chatId,
+            "❌ Помилка! Невірний формат IBAN.\nIBAN повинен починатися з 'UA' та містити 29 символів.\nБудь ласка, введіть коректний IBAN:",
+            {
+              reply_markup: {
+                inline_keyboard: [[{ text: "Назад", callback_data: "back" }]],
+              },
+            }
+          );
+        }
+      }
+
+      case "waitingForIPN": {
+        if (/^[1-9]\d{9}$/.test(text)) {
+          userState[chatId] = {
+            ...userState[chatId],
+            step: "waitingForName",
+            IBANdata: {
+              ...(userState[chatId]?.IBANdata || {}),
+              IPN: text,
+            },
+          };
+
+          return sendMessage(
+            chatId,
+            "Введіть прізвище, ім'я та по батькові власника рахунку:",
+            {
+              reply_markup: {
+                inline_keyboard: [
+                  [{ text: "Назад", callback_data: "back" }],
+                  [{ text: "Скасувати", callback_data: "back" }],
+                ],
+              },
+            }
+          );
+        } else {
+          return sendMessage(
+            chatId,
+            "❌ Помилка! Неправильний формат податкового номера.\nІПН повинен містити рівно 10 цифр.\nБудь ласка, введіть коректний ІПН:",
+            {
+              reply_markup: {
+                inline_keyboard: [[{ text: "Назад", callback_data: "back" }]],
+              },
+            }
+          );
+        }
+      }
+
+      case "waitingForName": {
+        if (
+          /^(?:[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ']+ ){2}[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ']+$/.test(
+            text
+          )
+        ) {
+          const IBANdata = {
+            ...(userState[chatId]?.IBANdata || {}),
+            name: text,
+          };
+
+          userState[chatId] = {
+            ...userState[chatId],
+            IBANdata,
+          };
+
+          await savePayments(chatId, JSON.stringify(IBANdata));
+          return showSummary(chatId, userState, currentState);
+        } else {
+          return sendMessage(
+            chatId,
+            "❌ Помилка! Невірний формат ПІБ.\nПрізвище, ім'я та по батькові повинні містити тільки літери українського або латинського алфавіту.\nПриклад: Іваненко Іван Іванович",
+            {
+              reply_markup: {
+                inline_keyboard: [[{ text: "Назад", callback_data: "back" }]],
+              },
+            }
+          );
+        }
+      }
+
+      default: {
+        userState[chatId] = { step: "idle" };
+        return sendMessage(
           chatId,
           "⚠️ Невідомий крок. Скиньте, будь ласка, команду ще раз."
         );
-        userState[chatId] = { step: "idle" };
-        break;
+      }
     }
-
-    return;
   }
 
   const handlers = createMessageHandlers(chatId);
 
-  text in handlers
+  return text in handlers
     ? await handlers[text as ValidCommand]()
     : await sendMessage(chatId, "Невідома команда.");
 };
