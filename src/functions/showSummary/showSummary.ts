0ac03ac6d6350c666d77ec2d6e6e2cd17d5018d1
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-23 10:45:42 +0300

    Delete if statement

--- src/functions/showSummary/showSummary.ts
+++ src/functions/showSummary/showSummary.ts
@@ -3,29 +3,25 @@ import { IshowSummary } from "./interface.js";
 import menu from "./menu.js";
 
 const showSummary: IshowSummary = async (chatId, userState, currentState) => {
-  const state = currentState;
+  userState[chatId] = {
+    ...currentState,
+    step: "confirmOrder",
+  };
 
-  if (state?.step === "showSummary") {
-    userState[chatId] = {
-      ...currentState,
-      step: "confirmOrder",
-    };
+  const { crypto, price, paymentMethod } = currentState;
 
-    const { crypto, price, paymentMethod } = currentState;
-
-    return sendMessage(
-      chatId,
-      `📦 Перегляд оголошення\n\n` +
-        `🔸 Оголошення N: 123456\n` +
-        `🔸 Криптовалюта: ${crypto}\n` +
-        `🔸 Ціна: ${price} UAH за 1 ${crypto}\n` +
-        `🔸 Валюта оплати: UAH\n` +
-        `🔸 Спосіб оплати: ${paymentMethod} \n` +
-        `🔸 Термін дії: 24 години \n` +
-        `✅ Все вірно?`,
-      menu
-    );
-  }
+  return sendMessage(
+    chatId,
+    `📦 Перегляд оголошення\n\n` +
+      `🔸 Оголошення N: 123456\n` +
+      `🔸 Криптовалюта: ${crypto}\n` +
+      `🔸 Ціна: ${price} UAH за 1 ${crypto}\n` +
+      `🔸 Валюта оплати: UAH\n` +
+      `🔸 Спосіб оплати: ${paymentMethod} \n` +
+      `🔸 Термін дії: 24 години \n` +
+      `✅ Все вірно?`,
+    menu
+  );
 };
 
 export default showSummary;
