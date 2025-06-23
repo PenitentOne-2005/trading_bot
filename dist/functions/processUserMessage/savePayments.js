3f0326a9d094a6e3bcfc3408c551876e08e3105c
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-23 11:00:05 +0300

    Dist

--- dist/functions/processUserMessage/savePayments.js
+++ dist/functions/processUserMessage/savePayments.js
@@ -8,8 +8,10 @@ const savePayments = async (chatId, metadata) => {
             metadata TEXT
           );
         `);
-        await pool.query(`INSERT INTO users (telegram_id, metadata)
+        await pool.query(`INSERT INTO payments (telegram_id, metadata)
            VALUES ($1, $2)
+           ON CONFLICT (telegram_id) DO UPDATE
+           SET metadata = EXCLUDED.metadata
           `, [chatId, metadata]);
         console.log(`✅ Пользователь ${chatId} сохранён.`);
     }
