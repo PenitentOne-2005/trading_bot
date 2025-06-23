48c36a12ef54d9798bfeb9e5ce72dc835fba48a6
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-23 10:29:38 +0300

    Dist

--- dist/functions/processUserMessage/processUserMessage.js
+++ dist/functions/processUserMessage/processUserMessage.js
@@ -32,8 +32,7 @@ const processUserMessage = async (msg) => {
                 if (/^\d{16}$/.test(text)) {
                     const obj = JSON.stringify({ text });
                     await savePayments(chatId, obj);
-                    // return showSummary(chatId, userState, currentState);
-                    return sendMessage(chatId, "Номер картки збережено!");
+                    return await showSummary(chatId, userState, currentState);
                 }
                 else {
                     return sendMessage(chatId, "❌ Помилка! Невірний номер картки.\nНомер банківської картки повинен містити рівно 16 цифр без пробілів або символів.\nБудь ласка, введіть коректний номер карти:", {
