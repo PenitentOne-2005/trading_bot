05ec99adad35a93b5e7966645080cf26730d99af
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-06-23 17:10:50 +0300

    Change to "amount"

--- src/functions/showSummary/showSummary.ts
+++ src/functions/showSummary/showSummary.ts
@@ -7,14 +7,14 @@ const showSummary: IshowSummary = async (chatId, userState) => {
 
   if (!currentState) return;
 
-  const { crypto, price, paymentMethod } = currentState;
+  const { crypto, amount, paymentMethod } = currentState;
 
   return sendMessage(
     chatId,
     `📦 Перегляд оголошення\n\n` +
       `🔸 Оголошення N: 123456\n` +
       `🔸 Криптовалюта: ${crypto}\n` +
-      `🔸 Ціна: ${price} UAH за 1 ${crypto}\n` +
+      `🔸 Ціна: ${amount} UAH за 1 ${crypto}\n` +
       `🔸 Валюта оплати: UAH\n` +
       `🔸 Спосіб оплати: ${paymentMethod} \n` +
       `🔸 Термін дії: 24 години \n` +
