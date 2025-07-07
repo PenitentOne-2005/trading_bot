385f5c1fb1059ca3bf970afd8f41b5a0151c4747
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-07 20:38:32 +0300

    New function

---
+++ src/functions/callbackHandlers/payMethod.ts
@@ -0,0 +1,37 @@
+import sendMessage from "../sendMessage/sendMessage.js";
+import showSummary from "../showSummary/showSummary.js";
+import { payMethodProps } from "./interface.js";
+
+const payMethod: payMethodProps = (props) => {
+  const { chatId, savedPayment, userState } = props;
+
+  if (savedPayment) {
+    userState[chatId] = {
+      ...userState[chatId],
+      paymentMethod: savedPayment,
+    };
+
+    return showSummary(chatId, userState);
+  }
+
+  userState[chatId] = {
+    ...userState[chatId],
+    step: "waitingForPaymentMethod",
+  };
+
+  return sendMessage(
+    chatId,
+    "У вас ще не збережено платіжний метод. Будь ласка, додайте:",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Банківська карта", callback_data: "card" }],
+          [{ text: "Банківський рахунок (IBAN)", callback_data: "IBAN" }],
+          [{ text: "Назад", callback_data: "back" }],
+        ],
+      },
+    }
+  );
+};
+
+export default payMethod;
