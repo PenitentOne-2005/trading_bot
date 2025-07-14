4aebbfa92deb122cdd3fd9b8e648cb062d2db1f1
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-14 19:24:43 +0300

    Update

--- src/functions/showOrders/showOrders.ts
+++ src/functions/showOrders/showOrders.ts
@@ -8,7 +8,6 @@ const showOrders: IShowOrders = async (params) => {
     const { userOffsets, chatId, dbName, text } = params;
 
     const offset = userOffsets[chatId] ?? 0;
-
     const query = `SELECT * FROM ${dbName} ORDER BY created_at ASC LIMIT 2 OFFSET $1`;
     const response = await pool.query(query, [offset]);
 
@@ -17,18 +16,36 @@ const showOrders: IShowOrders = async (params) => {
     }
 
     let messageText = `📄 ${text}:\n\n`;
+
+    const inline_keyboard = [];
+
     response.rows.forEach((order, index) => {
       const { id, username, crypto, amount, price, status } = order;
+
       messageText += `#${
         offset + index + 1
-      }\n Оголошення #${id}\n 👤 Пользователь: @${username}\n💱 Крипта: ${crypto}\n💰 Сумма: ${amount}\n💵 Цена: ${price}\n📌 Статус: ${status}\n\n`;
+      }\n Оголошення #${id}\n👤 @${username}\n💱 Крипта: ${crypto}\n💰 Сума: ${amount}\n💵 Ціна: ${price}\n📌 Статус: ${status}\n\n`;
+
+      inline_keyboard.push([
+        {
+          text: `Вибрати оголошення #${id}`,
+          callback_data: `select_order_${id}`,
+        },
+      ]);
     });
 
-    await sendMessage(chatId, messageText, ordersKeyBoard);
+    inline_keyboard.push([
+      { text: "⬅️ Попередня сторінка", callback_data: "buy_crypto_prev" },
+      { text: "➡️ Наступна сторінка", callback_data: "buy_crypto_next" },
+    ]);
+
+    sendMessage(chatId, messageText, {
+      reply_markup: { inline_keyboard },
+    });
 
     userOffsets[chatId] = offset + response.rows.length;
   } catch (error) {
-    console.log("❌ Ошибка при получении заявок:", error);
+    console.log("❌ Помилка при отриманні заявок:", error);
   }
 };
 
