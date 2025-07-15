bec2281855a6f702ee2c4a24fa9fa95c7d11de96
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-15 21:04:38 +0300

    .

--- src/functions/createExchangeMenu/interface.ts
+++
@@ -1,5 +0,0 @@
-import { Message } from "node-telegram-bot-api";
-
-export interface ICreateExchange {
-  (msg: Message): void;
-}
--- src/functions/showWalletBalance/interface.ts
+++
@@ -1,5 +0,0 @@
-import { Message } from "node-telegram-bot-api";
-
-export interface IShowWalletBalance {
-  (msg: Message): Promise<void>;
-}
