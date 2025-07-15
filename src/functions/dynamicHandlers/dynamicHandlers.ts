61b2329759251829aeab40c6617eb83cd7c4c08d
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-15 20:34:39 +0300

    Add "select_order_"

--- src/functions/dynamicHandlers/dynamicHandlers.ts
+++ src/functions/dynamicHandlers/dynamicHandlers.ts
@@ -1,12 +1,45 @@
+import { Message } from "node-telegram-bot-api";
+import pool from "../../db.js";
 import { CallbackProps } from "../../interface.js";
 import { userState } from "../../userState.js";
 import processBuyCryptoSelection from "../processBuyCryptoSelection/processBuyCryptoSelection.js";
+import sendMessage from "../sendMessage/sendMessage.js";
 
 const dynamicHandlers: {
-  [key: string]: (data: string, props: CallbackProps) => void | Promise<void>;
+  [key: string]: (
+    data: string,
+    props: CallbackProps
+  ) => void | Promise<void | Message>;
 } = {
   buy_: (data, props) => {
     processBuyCryptoSelection(data, props.chatId, userState);
+  },
+
+  select_order_: async (data, { chatId }) => {
+    const orderId = data.replace("select_order_", "");
+    const action = userState[chatId]?.currentDb;
+
+    if (!action) {
+      return sendMessage(chatId, "⚠️ Невідома таблиця заявок.");
+    }
+
+    const query = `SELECT * FROM ${action} WHERE id = $1`;
+    const result = await pool.query(query, [orderId]);
+
+    if (result.rows.length === 0) {
+      return sendMessage(chatId, "⚠️ Заявку не знайдено.");
+    }
+
+    const { amount, crypto, price } = result.rows[0];
+
+    const sumToPay = amount * price;
+
+    const actionText =
+      action === "buy_requests"
+        ? `🟢 Пiдтвердження\nВи збираєтесь *купити* ${amount} ${crypto} за ${sumToPay} UAH`
+        : `🔴 Пiдтвердження\nВи збираєтесь *продати* ${amount} ${crypto} за ${sumToPay} UAH`;
+
+    await sendMessage(chatId, `📝 ${actionText} #${orderId}`);
   },
 };
 
