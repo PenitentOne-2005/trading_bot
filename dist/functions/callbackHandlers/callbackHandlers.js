830df7b98230eeb1025b4a2788dd2c19504d1014
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-22 19:07:22 +0300

    Dist

--- dist/functions/callbackHandlers/callbackHandlers.js
+++ dist/functions/callbackHandlers/callbackHandlers.js
@@ -118,7 +118,7 @@ const callbackHandlers = {
         const query = `SELECT * FROM payments WHERE id = $1`;
         const result = await pool.query(query, [orderId]);
         if (result.rows.length != 0) {
-            return sendMessage(chatId, `Заявка: ${result.rows[0]}`);
+            return sendMessage(chatId, `Заявка: ${result.rows[0].metadata}`);
         }
     },
     show_payment_sell_info: () => { },
