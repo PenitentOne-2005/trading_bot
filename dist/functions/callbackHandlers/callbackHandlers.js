dbd5034c58e16c99b0004337bff775bda796a05b
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-24 19:18:18 +0300

    Dist

--- dist/functions/callbackHandlers/callbackHandlers.js
+++ dist/functions/callbackHandlers/callbackHandlers.js
@@ -117,8 +117,7 @@ const callbackHandlers = {
         const { orderId, amount, sumToPay } = userState[chatId] ?? {};
         const query = `SELECT * FROM payments WHERE id = $1`;
         const result = await pool.query(query, [orderId]);
-        const metadataRaw = result.rows[0].metadata;
-        const metadata = JSON.parse(metadataRaw);
+        const metadata = JSON.parse(result.rows[0].metadata);
         const text = `Надiшлiть ${sumToPay} UAH продавцю за наступними реквiзитами:\n\n Сума ${amount} USDT переведена в ескроу контракт, що очiкує пiдтвердження отримання оплати вiд продавця.\n Спосiб оплати: IBAN\n Номер IBAN: ${metadata.IBAN}\n Отримувач: ${metadata.name}\n Термiн дiï: 30хв\n Пiдтверждуєте, що надiслали кошти?`;
         const menu = {
             reply_markup: {
