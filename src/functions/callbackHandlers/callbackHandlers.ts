befbc2c886a585602309ebbbda187d62724f1f87
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-24 19:07:18 +0300

    Update

--- src/functions/callbackHandlers/callbackHandlers.ts
+++ src/functions/callbackHandlers/callbackHandlers.ts
@@ -184,7 +184,7 @@ const callbackHandlers: Record<
     const metadataRaw = result.rows[0].metadata;
     const metadata = JSON.parse(metadataRaw);
 
-    // const text = `Надiшлiть ${sumToPay} UAH продавцю за наступними реквiзитами:\n\n Сума ${amount} USDT переведена в ескроу контракт, що очiкує пiдтвердження отримання оплати вiд продавця.\n Спосiб оплати: IBAN\n Номер IBAN: ${metadata.IBAN}\n Отримувач: ${metadata.name}\n Термiн дiï: 30хв\n Пiдтверждуєте, що надiслали кошти?`;
+    const text = `Надiшлiть ${sumToPay} UAH продавцю за наступними реквiзитами:\n\n Сума ${amount} USDT переведена в ескроу контракт, що очiкує пiдтвердження отримання оплати вiд продавця.\n Спосiб оплати: IBAN\n Номер IBAN: ${metadata.IBAN}\n Отримувач: ${metadata.name}\n Термiн дiï: 30хв\n Пiдтверждуєте, що надiслали кошти?`;
 
     const menu = {
       reply_markup: {
@@ -195,7 +195,7 @@ const callbackHandlers: Record<
       },
     };
 
-    return sendMessage(chatId, `text: ${metadata}`, menu);
+    return sendMessage(chatId, `text: ${text}`, menu);
   },
 
   show_payment_sell_info: () => {},
