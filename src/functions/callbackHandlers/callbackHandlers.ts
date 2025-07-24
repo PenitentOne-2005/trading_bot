6312eb6a202546d027ac4910d13472989e45a42e
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-07-24 18:23:32 +0300

    Update

--- src/functions/callbackHandlers/callbackHandlers.ts
+++ src/functions/callbackHandlers/callbackHandlers.ts
@@ -181,10 +181,6 @@ const callbackHandlers: Record<
     const query = `SELECT * FROM payments WHERE id = $1`;
     const result = await pool.query(query, [orderId]);
 
-    if (result.rows.length === 0) {
-      console.error("Payment not found");
-    }
-
     const metadata = JSON.parse(result.rows[0]);
 
     // const text = `Надiшлiть ${sumToPay} UAH продавцю за наступними реквiзитами:\n\n Сума ${amount} USDT переведена в ескроу контракт, що очiкує пiдтвердження отримання оплати вiд продавця.\n Спосiб оплати: IBAN\n Номер IBAN: ${metadata.IBAN}\n Отримувач: ${metadata.name}\n Термiн дiï: 30хв\n Пiдтверждуєте, що надiслали кошти?`;
