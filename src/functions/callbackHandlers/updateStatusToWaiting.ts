0e8010cb5c6fe773618660b3270c6aef639d5306
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-01 19:41:25 +0300

    update

--- src/functions/callbackHandlers/updateStatusToWaiting.ts
+++ src/functions/callbackHandlers/updateStatusToWaiting.ts
@@ -10,7 +10,7 @@ const updateStatusToWaiting: FuncInfoProps = async (userState, chatId) => {
       return sendMessage(chatId, "❗ Заявка не знайдена або вже оброблена.");
     }
 
-    const selectQuery = `SELECT * FROM sell_requests; WHERE id = $1`;
+    const selectQuery = `SELECT * FROM sell_requests WHERE id = $1`;
     const result = await pool.query(selectQuery, [orderId]);
 
     if (result.rows.length === 0) {
@@ -18,7 +18,7 @@ const updateStatusToWaiting: FuncInfoProps = async (userState, chatId) => {
     }
 
     const updateQuery = `
-      UPDATE sell_requests;
+      UPDATE sell_requests
       SET status = 'waiting', buyer_chat_id = $1
       WHERE id = $2
     `;
