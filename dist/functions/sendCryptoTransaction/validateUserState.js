9f1b96fadc8ce6454e5fb92d3fd9e35347d118ea
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-04 22:27:45 +0300

    Dist

--- dist/functions/sendCryptoTransaction/validateUserState.js
+++ dist/functions/sendCryptoTransaction/validateUserState.js
@@ -1,8 +1,11 @@
 import { allowedKeys } from "./interface.js";
 import { userState } from "../../userState.js";
+const normalizeCrypto = (cryptoRaw) => {
+    return cryptoRaw?.toLowerCase().split(" ")[0].trim();
+};
 const validateUserState = (chatId) => {
     const { crypto, amount, sumToPay } = userState[chatId] ?? {};
-    const toLowerCrypto = crypto?.toLowerCase();
+    const toLowerCrypto = normalizeCrypto(crypto);
     if (!toLowerCrypto || !allowedKeys.includes(toLowerCrypto)) {
         throw new Error(`❌ Неверная или неуказанная криптовалюта. ${toLowerCrypto}, ${amount} ${sumToPay}`);
     }
