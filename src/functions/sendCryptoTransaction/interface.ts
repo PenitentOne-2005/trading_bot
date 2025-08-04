27d256d2042a07378119d9806890623e715ae1cc
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-04 17:44:48 +0300

    Create interface

---
+++ src/functions/sendCryptoTransaction/interface.ts
@@ -0,0 +1,25 @@
+import { Message } from "node-telegram-bot-api";
+import { TronWeb } from "tronweb";
+
+export const allowedKeys = ["trx", "usdt", "usdc", "tusd", "dai"] as const;
+export type CryptoKey = (typeof allowedKeys)[number];
+
+export interface SendCryptoTransaction {
+  (chatId: number): Promise<Message>;
+}
+
+export interface SendTRX {
+  (tronWeb: TronWeb, amount: number, chatId: number): Promise<Message>;
+}
+
+export interface SendTRC20 {
+  (props: SendTRC20Props): Promise<Message>;
+}
+
+interface SendTRC20Props {
+  tronWebUser: TronWeb;
+  crypto: string;
+  amount: number;
+  sumToPay: number | undefined;
+  chatId: number;
+}
