62a0131ea70b8c69e791a90bebd32c2982c731fb
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-04 17:46:26 +0300

    Create validateUserState

---
+++ src/functions/sendCryptoTransaction/validateUserState.ts
@@ -0,0 +1,21 @@
+import { allowedKeys, CryptoKey } from "./interface.js";
+import { userState } from "../../userState.js";
+
+const validateUserState = (chatId: number) => {
+  const { crypto, amount, sumToPay } = userState[chatId] ?? {};
+
+  const toLowerCrypto = crypto?.toLowerCase();
+  if (!toLowerCrypto || !allowedKeys.includes(toLowerCrypto as CryptoKey)) {
+    throw new Error(
+      `❌ Неверная или неуказанная криптовалюта. ${crypto}, ${amount} ${sumToPay}`
+    );
+  }
+
+  if (!amount || isNaN(amount) || amount <= 0) {
+    throw new Error("❌ Неверная или неуказанная сумма.");
+  }
+
+  return { crypto: toLowerCrypto, amount, sumToPay };
+};
+
+export default validateUserState;
