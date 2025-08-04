e7be002a9adec5b2109ee4e1def3815999ef4886
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-04 22:31:46 +0300

    Add condition in query

--- src/functions/showOrders/showOrders.ts
+++ src/functions/showOrders/showOrders.ts
@@ -11,11 +11,13 @@ const showOrders: IShowOrders = async (params) => {
 
     const query = `
       SELECT * FROM ${dbName}
+      WHERE chat_id != $2
+        AND status = 'active'
       ORDER BY created_at ASC
       LIMIT 2 OFFSET $1
     `;
 
-    const response = await pool.query(query, [offset]);
+    const response = await pool.query(query, [offset, chatId]);
 
     if (response.rows.length === 0) {
       return sendMessage(chatId, "📭 Пока нет заявок.");
