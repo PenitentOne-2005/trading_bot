47d97cba34f679dbaeab57b93f1f10cefa043362
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-04 16:20:01 +0300

    Update query, add props "chatId"

--- src/functions/address/getWalletAddress.ts
+++ src/functions/address/getWalletAddress.ts
@@ -1,13 +1,15 @@
 import pool from "../../db.js";
 import { IgetWalletAddress } from "./interface.js";
 
-const getWalletAddress: IgetWalletAddress = async () => {
+const getWalletAddress: IgetWalletAddress = async (chatId) => {
   try {
-    const res = await pool.query("SELECT wallet_address FROM users LIMIT 1");
+    const res = await pool.query(
+      "SELECT wallet_address FROM users WHERE telegram_id = $1",
+      [chatId]
+    );
 
     if (res.rows.length > 0) {
       const wallet = JSON.parse(res.rows[0].wallet_address);
-
       return wallet.base58;
     } else {
       throw new Error("❌ Кошелек не найден в базе");
