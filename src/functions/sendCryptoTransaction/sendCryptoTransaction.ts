6399ee44f6717909ee8ed2106dd8dbdbe1c92e3f
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-04 17:45:57 +0300

    Rewrite sendCryptoTransaction

--- src/functions/sendCryptoTransaction/sendCryptoTransaction.ts
+++ src/functions/sendCryptoTransaction/sendCryptoTransaction.ts
@@ -1,40 +1,23 @@
 import { TronWeb } from "tronweb";
 import dotenv from "dotenv";
+import { CryptoKey, SendCryptoTransaction } from "./interface.js";
 import sendMessage from "../sendMessage/sendMessage.js";
-import { userState } from "../../userState.js";
 import getWalletBalance from "../balance/getWalletBalance.js";
 import { getPrivateKeyFromDB } from "../encrypt/encryptPrivateKey.js";
-import { SendMessageOptions } from "node-telegram-bot-api";
+import { CONTRACTS } from "./dataTokens.js";
+import validateUserState from "./validateUserState.js";
+import sendTRC20 from "./sendTRC20.js";
+import sendTRX from "./sendTRX.js";
 
 dotenv.config();
 
-const ESCROW_ADDRESS = process.env.ESCROW_ADDRESS || "";
 const TRONGRID_API_KEY = process.env.TRONGRID_API_KEY!;
 
-const allowedKeys = ["trx", "usdt", "usdc", "tusd", "dai"] as const;
-type CryptoKey = (typeof allowedKeys)[number];
-
-const menu: SendMessageOptions = {
-  reply_markup: {
-    inline_keyboard: [
-      [
-        {
-          text: "📃 Пiдтвердити отримання грошей",
-          callback_data: "agree_get",
-        },
-      ],
-      [{ text: "ℹ️ Моï замовлення", callback_data: "myOrders" }],
-    ],
-  },
-};
-
-const sendCryptoTransaction = async (chatId: number) => {
+const sendCryptoTransaction: SendCryptoTransaction = async (chatId) => {
   try {
-    const privateKey = await getPrivateKeyFromDB(chatId); // достаешь приватник из БД
-
-    if (!privateKey) {
+    const privateKey = await getPrivateKeyFromDB(chatId);
+    if (!privateKey)
       throw new Error("❌ Приватный ключ не найден в базе данных.");
-    }
 
     const tronWebUser = new TronWeb({
       fullHost: "https://api.trongrid.io",
@@ -43,24 +26,11 @@ const sendCryptoTransaction = async (chatId: number) => {
     });
 
     const balance = await getWalletBalance(chatId);
-    if (!balance) {
-      throw new Error("❌ Не удалось получить баланс.");
-    }
+    if (!balance) throw new Error("❌ Не удалось получить баланс.");
 
-    const { crypto, amount, sumToPay } = userState[chatId] ?? {};
-    const toLowerCrypto = crypto?.toLowerCase();
+    const { crypto, amount, sumToPay } = validateUserState(chatId);
+    const balanceAmount = balance[crypto as CryptoKey];
 
-    if (!toLowerCrypto || !allowedKeys.includes(toLowerCrypto as CryptoKey)) {
-      throw new Error(
-        `❌ Неверная или неуказанная криптовалюта. ${crypto}, ${amount} ${sumToPay}`
-      );
-    }
-
-    if (!amount || isNaN(amount) || amount <= 0) {
-      throw new Error("❌ Неверная или неуказанная сумма.");
-    }
-
-    const balanceAmount = balance[toLowerCrypto as CryptoKey];
     if (balanceAmount < amount) {
       return sendMessage(
         chatId,
@@ -68,62 +38,21 @@ const sendCryptoTransaction = async (chatId: number) => {
       );
     }
 
-    // отправляем TRX
-    if (toLowerCrypto === "trx") {
-      const sunAmount = new BigNumber(tronWebUser.toSun(amount));
-      const result = await tronWebUser.trx.sendTransaction(
-        ESCROW_ADDRESS,
-        sunAmount.toNumber()
-      );
-
-      console.log("✅ Транзакция TRX отправлена:", result);
-      return sendMessage(chatId, `✅ Отправлено ${amount} TRX на эскроу.`);
+    if (crypto === "trx") {
+      return await sendTRX(tronWebUser, amount, chatId);
     }
 
-    // карта контрактов токенов TRC-20
-    const CONTRACTS: Record<string, string> = {
-      usdt: "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj",
-      usdc: "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8",
-      tusd: "TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT",
-      dai: "TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9",
-    };
+    if (!CONTRACTS[crypto]) throw new Error("❌ Контракт токена не найден.");
 
-    const TOKEN_DECIMALS: Record<string, number> = {
-      usdt: 6,
-      usdc: 6,
-      tusd: 18,
-      dai: 18,
-    };
-
-    const tokenAddress = CONTRACTS[toLowerCrypto];
-    const decimals = TOKEN_DECIMALS[toLowerCrypto];
-
-    if (!tokenAddress) {
-      throw new Error("❌ Контракт токена не найден.");
-    }
-
-    const contract = await tronWebUser.contract().at(tokenAddress);
-    const amountWithDecimals = new BigNumber(amount).multipliedBy(
-      10 ** decimals
-    );
-
-    const transferTx = await contract.methods
-      .transfer(ESCROW_ADDRESS, amountWithDecimals.toFixed())
-      .send();
-
-    return sendMessage(
+    const sendTRC20Props = {
+      tronWebUser,
+      crypto,
+      amount,
+      sumToPay,
       chatId,
-      `✅ Успiшно! Криптовалюта перемещiна в ескроу. Очiкує підтвердження вiд покупця про вiдправку коштiв.\n
-      
-      Оголошення #1001
-      Продали: ${amount}
-      Сума: ${sumToPay}
-      Статус: Виконується
-      Реквiзити для оплати переданi покупцевi.
-      Термін дiï: 30хв
-      ! На цьому етапi угоду скасувати неможливо, вона проходить через блокчейн.`,
-      menu
-    );
+    };
+
+    return await sendTRC20(sendTRC20Props);
   } catch (error: any) {
     console.error("❌ Ошибка при отправке:", error?.message || error);
     return sendMessage(
