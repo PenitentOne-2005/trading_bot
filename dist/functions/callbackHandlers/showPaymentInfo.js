0c3bc4c64554e03cd70e299505aef069aab276d4
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-11 18:06:24 +0300

    Dist

--- dist/functions/callbackHandlers/showPaymentInfo.js
+++ dist/functions/callbackHandlers/showPaymentInfo.js
@@ -2,9 +2,9 @@ import { showPaymentInfoKeyBoard } from "./menu.js";
 import pool from "../../db.js";
 import sendMessage from "../sendMessage/sendMessage.js";
 const showPaymentInfo = async (userState, chatId) => {
-    const { orderId, amount, sumToPay } = userState[chatId] ?? {};
-    const query = `SELECT * FROM payments WHERE id = $1`;
-    const result = await pool.query(query, [orderId]);
+    const { amount, sumToPay } = userState[chatId] ?? {};
+    const query = `SELECT * FROM payments WHERE telegram_id = $1`;
+    const result = await pool.query(query, [chatId]);
     const metadata = JSON.parse(result.rows[0].metadata);
     const text = `Надiшлiть ${sumToPay} UAH продавцю за наступними реквiзитами:\n\n Сума ${amount} USDT переведена в ескроу контракт, що очiкує пiдтвердження отримання оплати вiд продавця.\n Спосiб оплати: IBAN\n Номер IBAN: ${metadata.IBAN}\n Отримувач: ${metadata.name}\n Термiн дiï: 30хв\n Пiдтверждуєте, що надiслали кошти?`;
     userState[chatId] = {
