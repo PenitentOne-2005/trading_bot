b409a1dee4fadf18e69df876e6e09fa068ec052f
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-12 19:43:46 +0300

    Update condition

--- src/functions/dynamicHandlers/confirmOrderPreview.ts
+++ src/functions/dynamicHandlers/confirmOrderPreview.ts
@@ -2,6 +2,7 @@ import { Props } from "./interface.js";
 import pool from "../../db.js";
 import sendMessage from "../sendMessage/sendMessage.js";
 import { userState } from "../../userState.js";
+import { showPaymentBuy, showPaymentSell } from "./menu.js";
 
 const confirmOrderPreview: Props = async (action, chatId, orderId) => {
   if (!action) {
@@ -24,34 +25,7 @@ const confirmOrderPreview: Props = async (action, chatId, orderId) => {
       ? `🔴 Пiдтвердження\nВи збираєтесь *продати* ${amount} ${crypto} за ${sumToPay} UAH\n Пiсля пiдтвердження криптовалюта буде перемiщена в ескроу-контракт.\n Термiн дiї: 30хв\n Активи зберігаються там підтвердження отримання платежу і автоматично переказуються смартконтрактом отримувачу після повного підтвердження операції.\n\n При виникненні проблеми ви матимете можливість оскаржити операцію. Скарга буде розглянута спільнотою.\n • Підтверджуєте переміщення коштів у ескроу?`
       : `🟢 Пiдтвердження\nВи збираєтесь *купити* ${amount} ${crypto} за ${sumToPay} UAH\n \n Наступний крок: Ви маєте надiслати ${sumToPay} UAH продавцю та пiдтвердити оплату.\n Термiн дiї: 30хв\n Пiдтверджуєте, що хочете продовжити?`;
 
-  const menu =
-    action === "buy_requests"
-      ? {
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "Так, підтверджую",
-                  callback_data: "show_payment_sell_info",
-                },
-              ],
-              [{ text: "Назад", callback_data: "back" }],
-            ],
-          },
-        }
-      : {
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "Так, переглянути реквiзити для оплати",
-                  callback_data: "show_payment_buy_info",
-                },
-              ],
-              [{ text: "Назад", callback_data: "back" }],
-            ],
-          },
-        };
+  const menu = action === "buy_requests" ? showPaymentSell : showPaymentBuy;
 
   userState[chatId] = {
     ...userState[chatId],
