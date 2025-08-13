88c579702c892e5eb2ba48a99d5ba8bc89145e42
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-13 18:39:47 +0300

    Update query

--- src/functions/callbackHandlers/cancelPaymentProcess.ts
+++ src/functions/callbackHandlers/cancelPaymentProcess.ts
@@ -9,16 +9,16 @@ const cancelPaymentProcess: FuncInfoProps = async (
   orderId
 ) => {
   try {
+    const { amount, sumToPay, crypto, currentDb } = userState[chatId] ?? {};
+
     const updateQuery = `
-        UPDATE sell_requests
+        UPDATE ${currentDb}
         SET status = 'active',
             buyer_chat_id = NULL
         WHERE id = $1 AND buyer_chat_id = $2
       `;
 
     await pool.query(updateQuery, [orderId, chatId]);
-
-    const { amount, sumToPay, crypto } = userState[chatId] ?? {};
 
     const text = `❌ Операцiю скасовано!\n Ви скасували покупку ${amount} ${crypto} за ${sumToPay} UAH.\n Оголошення #${orderId} залишилось активним, i ви можете використати його пiзнiше.\n Що далi?`;
 
