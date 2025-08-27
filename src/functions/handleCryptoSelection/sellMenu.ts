9fb0047a50ede225c7eb6898ace15453b450e70f
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-27 19:31:46 +0300

    Add "TRX"

--- src/functions/handleCryptoSelection/sellMenu.ts
+++ src/functions/handleCryptoSelection/sellMenu.ts
@@ -7,6 +7,7 @@ export const sellMenu: SendMessageOptions = {
       [{ text: "USDC (TRC-20)", callback_data: "buy_USDC" }],
       [{ text: "TUSD (TRC-20)", callback_data: "buy_TUSD" }],
       [{ text: "DAI (TRC-20)", callback_data: "buy_DAI" }],
+      [{ text: "TRX (TRC-20)", callback_data: "buy_TRX" }],
       [{ text: "Назад", callback_data: "back" }],
     ],
   },
