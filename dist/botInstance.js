c21366e12f991fd7be332dd0586a2fb9f518d064
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-30 18:39:34 +0300

    Dist

--- dist/botInstance.js
+++ dist/botInstance.js
@@ -6,5 +6,5 @@ if (!token) {
     console.error("❌ BOT_TOKEN не найден! Убедитесь, что он задан в .env файле.");
     process.exit(1);
 }
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
 export default bot;
