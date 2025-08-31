50546ea48c95e869d0190401134591937ad169fd
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-31 20:43:26 +0300

    Add "Props, GetTokenBalance"

--- src/functions/balance/interface.ts
+++ src/functions/balance/interface.ts
@@ -1,8 +1,22 @@
+import { TronWeb } from "tronweb";
+
 export interface IgetWalletBalance {
   (chatId: number): Promise<{
     trx: number;
     usdt: number;
     usdc: number;
     tusd: number;
-} | null>
+  } | null>;
+}
+
+interface Props {
+  chatId: number;
+  tronWeb: TronWeb;
+  walletAddress: string | undefined;
+  tokenAddress: string;
+  decimals: number;
+}
+
+export interface GetTokenBalance {
+  (props: Props): Promise<number>;
 }
