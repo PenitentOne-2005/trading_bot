fb3895b0ff85ee709ed98c32c5b86bdad28290e1
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-08-31 17:29:33 +0300

    .

--- src/functions/balance/getWalletBalance.ts
+++ src/functions/balance/getWalletBalance.ts
@@ -23,17 +23,24 @@ const getWalletBalance: IgetWalletBalance = async (chatId) => {
 
     // Баланс TRC20 токенов
     const getTokenBalance = async (tokenAddress: string, decimals: number) => {
-      const contract = await tronWeb.contract().at(tokenAddress);
-      const balance = await contract.balanceOf(walletAddress).call({
-        from: walletAddress,
-      });
-      return Number(balance.toString()) / Math.pow(10, decimals);
+      try {
+        const contract = await tronWeb.contract().at(tokenAddress);
+        const balance = await contract.balanceOf(walletAddress).call();
+        return Number(balance.toString()) / Math.pow(10, decimals);
+      } catch (e: any) {
+        console.error(
+          `Ошибка при получении токена ${tokenAddress}:`,
+          e.message
+        );
+        sendMessage(chatId, `❌ Ошибка при получении токена ${tokenAddress}`);
+        return 0;
+      }
     };
 
     const [usdt, usdc, tusd] = await Promise.all([
       getTokenBalance(USDT_CONTRACT, 6),
       getTokenBalance(USDC_CONTRACT, 6),
-      getTokenBalance(TUSD_CONTRACT, 18), // TUSD тоже 6 знаков
+      getTokenBalance(TUSD_CONTRACT, 6),
     ]);
 
     return {
