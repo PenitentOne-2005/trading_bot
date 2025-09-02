669faa9d2409e711b26b7097ebec8430246e797d
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-02 18:00:23 +0300

    Create new

---
+++ src/functions/cryptoWithdraw/cryptoWithdraw.ts
@@ -0,0 +1,10 @@
+import { CryptoWithdraw } from "./interface.js";
+import { sendMessage } from "@/functions/index.js";
+
+const cryptoWithdraw: CryptoWithdraw = async (props) => {
+  const { userState, chatId, text } = props;
+
+  return sendMessage(chatId, "Успешно выведенно");
+};
+
+export default cryptoWithdraw;
