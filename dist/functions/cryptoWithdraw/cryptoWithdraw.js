91e40a6779d4180de72852d3463f56422578928b
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-02 18:31:13 +0300

    Dist

--- dist/functions/cryptoWithdraw/cryptoWithdraw.js
+++ dist/functions/cryptoWithdraw/cryptoWithdraw.js
@@ -1,7 +1,6 @@
 import { sendMessage } from "../../functions/index.js";
 const cryptoWithdraw = async (props) => {
     const { userState, chatId, text } = props;
-    userState[chatId] = { step: "idle" };
     return sendMessage(chatId, "Успешно выведенно");
 };
 export default cryptoWithdraw;
---
+++ dist/functions/cryptoWithdraw/menu.js
@@ -0,0 +1,5 @@
+export const menu = {
+    reply_markup: {
+        inline_keyboard: [[{ text: "Назад", callback_data: "back" }]],
+    },
+};
