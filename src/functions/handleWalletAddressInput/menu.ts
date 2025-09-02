557179d378afc7b6ce5770690d134007ce209e7d
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-02 18:03:22 +0300

    .

--- src/functions/handleWalletAddressInput/menu.ts
+++ src/functions/handleWalletAddressInput/menu.ts
@@ -3,7 +3,7 @@ import { SendMessageOptions } from "node-telegram-bot-api";
 export const menu: SendMessageOptions = {
   reply_markup: {
     inline_keyboard: [
-      [{ text: "TRX", callback_data: "withdraw_" }],
+      [{ text: "TRX", callback_data: "withdraw_TRX" }],
       [{ text: "Назад", callback_data: "back" }],
     ],
   },
