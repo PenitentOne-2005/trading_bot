9e2dd35609bd5505766c672bfb645db7967097ab
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-21 17:35:11 +0300

    Exported "formatBalancesToString, getAvailableUserTokens"

--- src/functions/handleWalletAddressInput/index.ts
+++ src/functions/handleWalletAddressInput/index.ts
@@ -1,5 +1,6 @@
 export { default } from "./handleWalletAddressInput.js";
 export { default as tokens } from "./tokens.js";
 export { default as tronWeb } from "./InitTronWeb.js";
-export { default as getAvailableUserBalances } from "./getAvailableUserBalances.js";
 export { default as initMenu } from "./initMenu.js";
+export { default as formatBalancesToString } from "./formatBalancesToString.js";
+export { default as getAvailableUserTokens } from "./getAvailableUserTokens.js";
