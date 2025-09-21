698726afe98f75703f920c0d42a44dde8b60e1b7
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-21 16:53:18 +0300

    Added "tokens, tronWeb, getAvailableUserBalances, initMenu"

--- src/functions/handleWalletAddressInput/index.ts
+++ src/functions/handleWalletAddressInput/index.ts
@@ -1,1 +1,5 @@
 export { default } from "./handleWalletAddressInput.js";
+export { default as tokens } from "./tokens.js";
+export { default as tronWeb } from "./InitTronWeb.js";
+export { default as getAvailableUserBalances } from "./getAvailableUserBalances.js";
+export { default as initMenu } from "./initMenu.js";
