9016e8c745a8dac14f91e15a694b470f4767461e
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-21 16:55:59 +0300

    Add interface

--- src/handleCallbackQuery/handleCallbackQuery.ts
+++ src/handleCallbackQuery/handleCallbackQuery.ts
@@ -1,20 +1,19 @@
-import { dynamicHandlers, callbackHandlers, CallbackProps } from "@/exports.js";
+import { handleCallbackQuery } from "./interface.js";
+import { dynamicHandlers, callbackHandlers } from "@/exports.js";
 import { sendMessage } from "@/functions/index.js";
 
-const handleCallbackQuery = async (data: string, props: CallbackProps) => {
+const handleCallbackQuery: handleCallbackQuery = async (data, props) => {
   if (callbackHandlers[data]) {
     return callbackHandlers[data](props);
   }
 
-  // Проверка на динамический префикс
   for (const prefix in dynamicHandlers) {
     if (data.startsWith(prefix)) {
       return dynamicHandlers[prefix](data, props);
     }
   }
 
-  // По умолчанию — неизвестная команда
-  await sendMessage(props.chatId, "❓ Невідома команда.");
+  sendMessage(props.chatId, "❓ Невідома команда.");
 };
 
 export default handleCallbackQuery;
