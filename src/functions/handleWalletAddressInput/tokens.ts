a77c71bcbd1129fe21ff6020ccf716119ccda674
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-09-21 16:51:41 +0300

    Create tokens.ts

---
+++ src/functions/handleWalletAddressInput/tokens.ts
@@ -0,0 +1,8 @@
+const tokens = [
+  { key: "USDT", label: "USDT" },
+  { key: "TRX", label: "TRX" },
+  { key: "USDC", label: "USDC" },
+  { key: "TUSD", label: "TUSD" },
+] as const;
+
+export default tokens;
