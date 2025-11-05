4c53bd56cb11d4a0737d89bb9415e7c7e1af6b95
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-11-06 00:16:24 +0300

    Dist

--- dist/functions/handleCryptoSelection/handleCryptoSelection.js
+++ dist/functions/handleCryptoSelection/handleCryptoSelection.js
@@ -3,15 +3,16 @@ import { sendMessage } from "../../functions/index.js";
 const handleCryptoSelection = (obj) => {
     const { chatId, text, CRYPTOS, userState } = obj;
     const { orderType } = userState[chatId];
+    const action = orderType === "buy" ? "купити" : "продати";
     if (!CRYPTOS.includes(text)) {
-        return sendMessage(chatId, `Виберіть криптовалюту, яку хочете ${orderType === "buy" ? "купити" : "продати"}:`, sellMenu);
+        return sendMessage(chatId, `Виберіть криптовалюту, яку хочете ${action}:`, sellMenu);
     }
     userState[chatId] = {
         ...userState[chatId],
         step: "waitingForPrice",
         crypto: text,
     };
-    return sendMessage(chatId, `💰 Вкажіть суму в ${text}, яку хочете ${orderType === "buy" ? "купити" : "продати"}:`, {
+    return sendMessage(chatId, `💰 Вкажіть суму в ${text}, яку хочете ${action}:`, {
         reply_markup: {
             inline_keyboard: [[{ text: "Назад", callback_data: "back" }]],
         },
