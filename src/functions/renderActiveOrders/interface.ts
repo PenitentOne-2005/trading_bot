d9cd03f8fc3007cfb64c5a2406dce194605f9d9f
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-01-03 18:09:59 +0300

    Update currentDb

--- src/functions/renderActiveOrders/interface.ts
+++ src/functions/renderActiveOrders/interface.ts
@@ -1,5 +1,5 @@
 import { Message } from "node-telegram-bot-api";
 
 export interface GetActiveOrders {
-  (chatId: number, currentDb?: string): Promise<Message | undefined>;
+  (chatId: number, currentDb: string | undefined): Promise<Message | undefined>;
 }
