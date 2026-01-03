db47455ee38c39925bee7528fe124b6f9a1e4042
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-01-03 17:37:09 +0300

    Add "unpublish_", "delete_"

--- src/dynamicHandlers/dynamicHandlers.ts
+++ src/dynamicHandlers/dynamicHandlers.ts
@@ -1,5 +1,7 @@
-import { userState } from "@/exports.js";
 import { DynamicHandlers } from "./interface.js";
+import { pool, userState } from "@/exports.js";
+import { sendMessage } from "@/functions/index.js";
+import { unPublishMenu } from "./unPublishMenu.js";
 
 const dynamicHandlers: DynamicHandlers = {
   buy_: async (data, { chatId }) => {
@@ -24,9 +26,7 @@ const dynamicHandlers: DynamicHandlers = {
   },
 
   agree_get_: async (data, { chatId }) => {
-    const { sendMessage, handleConfirmFiat } = await import(
-      "@/functions/index.js"
-    );
+    const { handleConfirmFiat } = await import("@/functions/index.js");
 
     try {
       const orderId = data.replace("agree_get_", "");
@@ -39,7 +39,80 @@ const dynamicHandlers: DynamicHandlers = {
       await handleConfirmFiat(chatId, orderId);
     } catch (err) {
       console.error("Ошибка в agree_get_:", err);
-      await sendMessage(chatId, "❌ Произошла ошибка при обработке.");
+      sendMessage(chatId, "❌ Произошла ошибка при обработке.");
+    }
+  },
+
+  unpublish_: async (data, { chatId }) => {
+    const { currentDb } = userState[chatId];
+
+    const orderId = data.replace("unpublish_", "");
+
+    const { rowCount } = await pool.query(
+      `SELECT 1 FROM ${currentDb} WHERE id = $1`,
+      [orderId]
+    );
+
+    if (!rowCount) {
+      return sendMessage(chatId, "❌ Оголошення не знайдено або вже видалене");
+    }
+
+    await pool.query(
+      `UPDATE ${currentDb}
+       SET status = 'unpublished'
+       WHERE id = $1`,
+      [orderId]
+    );
+
+    sendMessage(
+      chatId,
+      `✅ Оголошення успiшно знято з публiкацiї.
+Оголошення №${orderId} бiльше не вiдображається у списку активних.
+
+Що далi?`,
+      unPublishMenu
+    );
+  },
+
+  delete_: async (data, { chatId }) => {
+    try {
+      const { currentDb } = userState[chatId];
+
+      const orderId = data.replace("delete_", "");
+
+      const { rowCount } = await pool.query(
+        `
+      SELECT 1
+      FROM ${currentDb}
+      WHERE id = $1 AND chat_id = $2 AND status != 'deleted'
+      `,
+        [orderId, chatId]
+      );
+
+      if (!rowCount) {
+        return sendMessage(
+          chatId,
+          "❌ Оголошення не знайдено або воно вже видалене."
+        );
+      }
+
+      await pool.query(
+        `
+      UPDATE ${currentDb}
+      SET status = 'deleted'
+      WHERE id = $1
+      `,
+        [orderId]
+      );
+
+      sendMessage(
+        chatId,
+        `🗑️ Оголошення №${orderId} успішно видалено. Всi данi про це оголошення були видаленi. Що далi?`,
+        unPublishMenu
+      );
+    } catch (err) {
+      console.error("❌ Помилка в delete_:", err);
+      sendMessage(chatId, "⚠️ Сталася помилка при видаленні оголошення.");
     }
   },
 };
