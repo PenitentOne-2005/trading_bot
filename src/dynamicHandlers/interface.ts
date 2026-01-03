1863a368e481629cb5249f5dc87cfe4f729cb30e
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-01-03 17:36:39 +0300

    Update DynamicHandlers

--- src/dynamicHandlers/interface.ts
+++ src/dynamicHandlers/interface.ts
@@ -1,5 +1,9 @@
+import { Message } from "node-telegram-bot-api";
 import { CallbackProps } from "@/exports.js";
 
 export interface DynamicHandlers {
-  [key: string]: (data: string, props: CallbackProps) => Promise<void>;
+  [key: string]: (
+    data: string,
+    props: CallbackProps
+  ) => Promise<Message | void>;
 }
